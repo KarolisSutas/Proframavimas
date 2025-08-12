e6e6deef99f2a319890931ecaecb4e1a2c218d3f
KarolisSutas <sutaskarolis@gmail.com>
2025-08-12 11:08:41 +0300

    api su mentorium

---
+++ Mentorius/008 kazkas/index.js
@@ -0,0 +1,74 @@
+import express from 'express';
+
+const app = express();
+
+// CRUD
+// create - app.post()
+// read - app.get()
+// update - app.put()
+// delete - app.delete()
+
+app.get('/', (req, res) => {
+    res.send('Its alive! ITS ALIVE!!!');
+});
+
+app.get('/demo', (req, res) => {
+    res.send([1, 2, 3]);
+});
+
+// Params
+app.get('/demo/:id', (req, res) => {
+    const id = req.params.id;
+    res.send(id);
+});
+
+app.get('/demo/:id/:cat', (req, res) => {
+    const id = req.params.id;
+    const cat = req.params.cat;
+    res.send([id, cat]);
+
+});
+
+// queries
+
+app.get('/query', (req, res) => {
+    const q = req.query
+    res.send(q);
+});
+//...........................................
+
+// laikina duombaze
+const books = [
+    {
+        id: 1,
+        author: "Kobo Abe",
+        title: "Žmogus-dėžė",
+    },
+    {
+        id: 2,
+        author: "Iain Banks",
+        title: "Tiltas",
+    },
+    {
+        id: 3,
+        author: "Italo Calvino",
+        title: "Mūsų protėviai",
+    },
+];
+
+app.get('/api/books', (req, res) => {
+    res.send(books);
+});
+
+app.get('/api/books/:id', (req, res) => {
+    const id = req.params.id;
+    const book = books.find(book => book.id === parseInt(id));
+    if (!book) return res.status(404).send('Knyyga su tokiu id neegzistuoja');
+    res.status(200).send(book);
+
+});
+
+//............................................
+app.listen(3000, () => {
+    console.log('listening on port 3000...');
+});
